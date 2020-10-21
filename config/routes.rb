86a8e919d350dbdf9cd104ac84882180ad6724b6
BachShinichiroe <kitchen.number@gmail.com>
2020-10-21 17:01:52 +0900

    [add]予約時のメール送信機能と中間テーブル追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :events
+  resources :users
   root 'events#index'
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
