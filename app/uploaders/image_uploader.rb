d31a98619b5c89ceb3e4991b16e3a9fb1596107f
BachShinichiroe <kitchen.number@gmail.com>
2020-10-27 08:15:11 +0900

    [fix]アップロードするサイズを大きく

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,7 +6,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
-  process :resize_to_limit => [50, 50]
+  process :resize_to_limit => [500, 500]
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
