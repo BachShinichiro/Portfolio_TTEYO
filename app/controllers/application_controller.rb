a4678654923fd46045b332652a5784aae764b9df
BachShinichiroe <kitchen.number@gmail.com>
2020-10-28 05:30:31 +0900

    [fix]ログイン、ログアウト後の遷移指定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(resource)
+    events_path # ログイン後に遷移するpathを設定
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path # ログアウト後に遷移するpathを設定
+  end
 end
+
