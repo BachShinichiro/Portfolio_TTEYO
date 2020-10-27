5b53dec4c6baee8d61a7a1b82483f9e99d0f7c13
BachShinichiroe <kitchen.number@gmail.com>
2020-10-27 14:05:37 +0900

    [fix]routeを修正、Eventのformを修正

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,4 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
-  validates :password, presence: true, length: { minimum: 4 }
-  validates :name,  presence: true, length: { maximum: 30 }
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
