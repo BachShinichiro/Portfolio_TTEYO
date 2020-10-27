ea916035a287272dfb65e5ada7add420ec3749f7
BachShinichiroe <kitchen.number@gmail.com>
2020-10-28 07:49:16 +0900

    [fix]Deviceユーザーのカラム追加

--- db/migrate/20201027221412_add_name_profile_to_users.rb
+++ db/migrate/20201027221412_add_name_profile_to_users.rb
@@ -1,4 +1,4 @@
-class AddColumnsToUsers < ActiveRecord::Migration[5.2]
+class AddNameProfileToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :name, :string
     add_column :users, :client, :boolean, default: false
