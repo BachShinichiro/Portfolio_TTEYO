04b59ef8baae1d3484a40d0b1553f4f4dd705a8e
BachShinichiroe <kitchen.number@gmail.com>
2020-10-28 15:11:57 +0900

    [fix]登録時に名前登録、クライアント登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :events, dependent: :destroy
   has_many :reservations, dependent: :destroy
   validates :password, presence: true, length: { minimum: 4 }
