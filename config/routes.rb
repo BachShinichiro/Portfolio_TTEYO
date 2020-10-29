699c72f74001175485500555e52300be5286ad2f
BachShinichiroe <kitchen.number@gmail.com>
2020-10-30 01:41:04 +0900

    [fix]letter-openerをテスト環境

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :comments
   end
   root 'tops#index'
-  if Rails.env.development?
+  unless Rails.env.production?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 end
