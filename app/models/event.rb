82e05a0a28c2f85537dc61179b3a32f6ff980227
BachShinichiroe <kitchen.number@gmail.com>
2020-10-29 16:48:48 +0900

    [fix]enumを修正

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,7 +5,6 @@ class Event < ApplicationRecord
   mount_uploader :flyer, ImageUploader
 
   enum status: { 公演前: 0, 公演中: 1, 公演後: 2 }
-  enum tikcket_type: { 一般席: 0, 特別席: 1, 招待: 2 }
 
   validates :event_name,  presence: true, length: { maximum: 30 }
 end
