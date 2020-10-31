31cbca4e4e27a867b8b50d01337a96935d5df98d
BachShinichiroe <kitchen.number@gmail.com>
2020-10-31 10:41:10 +0900

    [add]コメント編集、削除機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
+  before_action :set_event, only: [:create, :edit, :update]
+
   def create
-    @event = Event.find(params[:event_id])
     @comment = @event.comments.build(comment_params)
     respond_to do |format|
       if @comment.save
@@ -10,8 +11,42 @@ class CommentsController < ApplicationController
       end
     end
   end
+
+  def edit
+    @comment = @event.comments.find(params[:id])
+    respond_to do |format|
+      flash.now[:notice] = 'コメントの編集中'
+      format.js { render :edit }
+    end
+  end
+
+  def update
+    @comment = @event.comments.find(params[:id])
+      respond_to do |format|
+        if @comment.update(comment_params)
+          flash.now[:notice] = 'コメントが編集されました'
+          format.js { render :index }
+        else
+          flash.now[:notice] = 'コメントの編集に失敗しました'
+          format.js { render :edit_error }
+        end
+      end
+  end
+  
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    respond_to do |format|
+      flash.now[:notice] = 'コメントが削除されました'
+      format.js { render :index }
+    end
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:event_id, :content)
+  end
+  def set_event
+    @event = Event.find(params[:event_id])
   end
 end
