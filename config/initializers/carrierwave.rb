80c0ed88d8ec93b725795b6567ad6bd5a08472f2
BachShinichiroe <kitchen.number@gmail.com>
2021-02-14 17:42:08 +0900

    fix:ディレクトリィ

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,7 +14,7 @@ CarrierWave.configure do |config|
     }
     config.fog_public     = true
     config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
-    config.fog_directory = 'tteyo-development'
+    config.fog_directory = 'tteyo-production'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tteyo-production	'
   else
       config.storage :file
