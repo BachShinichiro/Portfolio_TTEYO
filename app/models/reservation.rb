d04a47872bd88209fa8645fe095c65eff3fcf6ca
BachShinichiroe <kitchen.number@gmail.com>
2021-05-15 02:25:14 +0900

    ♻️:期間のすぎた公演が予約できないように

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -7,7 +7,7 @@ class Reservation < ApplicationRecord
   validates :number_of_ticket, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 20}
 
   def date_not_before_today
-    errors.add(:date, "は今日以降のものを選択してください") if date.nil? || date <= Date.today
+    errors.add(:date, "は今日以降のものを選択してください。またイベントは終了しています") if date.nil? || date <= Date.today
   end
   enum status: { 一般席: 0, 招待席: 1 }
 end
